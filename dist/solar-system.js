2cc8f75721cd92cf3b4d9c0c55646f553a6fede2
unknown <eedl@PCMAD229.gft.com>
2015-08-24 17:00:34 +0200

    Web Solar System with Three.js : bump v0.0.15

--- dist/solar-system.js
+++ dist/solar-system.js
@@ -1,7 +1,7 @@
 /*
  * solar-system
  * @Description Solar System with Threejs
- * @version v0.0.14 - 2015-08-24
+ * @version v0.0.15 - 2015-08-24
  * @link https://github.com/KenEDR/three-solar-system#readme
  * @author Enrique Daimiel Ruiz <k.daimiel@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
